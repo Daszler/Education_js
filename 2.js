c8646be3ee5a5f7efa824fec5392fa89f6f0fea8
Michael <mich.chausov@gmail.com>
2023-07-16 22:05:17 +0300

    2js commit

---
+++ 2.js
@@ -0,0 +1,15 @@
+function greeting() {
+    const name = prompt('Введите ваше имя');
+    console.log(`Привет, ${name}!`)
+}
+greeting()
+
+// function greetting() {
+//     let name = prompt('Введите ваше имя');
+//     str = `Привет, ${name}!!`
+//     alert(str)
+// }
+
+// greetting();
+// console.log(str);
+
